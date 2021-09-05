e5a5e51a5059775e1ecb92dae8c88b0e7f55dcc3
majbc1999 <majbc1999@gmail.com>
2021-09-05 18:06:25 +0200

    manjsi popravki

---
+++ orodja/napovedovanje.py
@@ -0,0 +1,40 @@
+from rezultat import vrni_napoved
+
+# Tukaj nastavimo matchday
+matchday = '1'
+
+# Tukaj napišemo popularne rezultate
+sez01 = ["0:2", "0:2", "1:3", "0:4"]
+sez02 = ["2:1", "2:0", "1:0", "1:1"]
+sez03 = ["1:2", "1:1", "0:1", "0:2"]
+sez04 = ["1:2", "1:3", "", ""]
+sez05 = ["1:1", "1:1", "1:1", "1:1"]
+sez06 = ["1:1", "1:1", "1:1", "1:1"]
+sez07 = ["1:1", "1:1", "1:1", "1:1"]
+sez08 = ["1:1", "1:1", "1:1", "1:1"]
+sez09 = ["1:1", "1:1", "1:1", "1:1"]
+sez10 = ["1:1", "1:1", "1:1", "1:1"]
+sez11 = ["1:1", "1:1", "1:1", "1:1"]
+sez12 = ["1:1", "1:1", "1:1", "1:1"]
+sez13 = ["1:1", "1:1", "1:1", "1:1"]
+sez14 = ["1:1", "1:1", "1:1", "1:1"]
+sez15 = ["1:1", "1:1", "1:1", "1:1"]
+sez16 = ["1:1", "1:1", "1:1", "1:1"]
+
+# Tukaj izračuna vse
+vrni_napoved(sez01, '01', matchday)
+vrni_napoved(sez02, '02', matchday)
+vrni_napoved(sez03, '03', matchday)
+vrni_napoved(sez04, '04', matchday)
+vrni_napoved(sez05, '05', matchday)
+vrni_napoved(sez06, '06', matchday)
+vrni_napoved(sez07, '07', matchday)
+vrni_napoved(sez08, '08', matchday)
+vrni_napoved(sez09, '09', matchday)
+vrni_napoved(sez10, '10', matchday)
+vrni_napoved(sez11, '11', matchday)
+vrni_napoved(sez12, '12', matchday)
+vrni_napoved(sez13, '13', matchday)
+vrni_napoved(sez14, '14', matchday)
+vrni_napoved(sez15, '15', matchday)
+vrni_napoved(sez16, '16', matchday)
