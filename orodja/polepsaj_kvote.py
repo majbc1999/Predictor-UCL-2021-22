10b80a4f9456effe727c9e16ea289790c390a479
majbc1999 <majbc1999@gmail.com>
2021-10-19 10:33:13 +0200

    zakomentiran del o ekipah

--- orodja/polepsaj_kvote.py
+++ orodja/polepsaj_kvote.py
@@ -7,27 +7,27 @@ def polepsaj_kvote(datoteka, matchday):
     ekipa2 = kvote[1]
     slovar = kvote[2]
 
-    vzorec_za_polepsanje_slovarja = re.compile(
-        r'(?P<ekipa>[a-zA-Z\s]*?)'
-        r' '
-        r'(?P<golivec>\d*?)'
-        r'-'
-        r'(?P<golimanj>\d*)',
-        flags=re.DOTALL)
-
-    polepsan_slovar = {}
-
-    for kljuc in slovar:
-        for kl in re.finditer(vzorec_za_polepsanje_slovarja, kljuc):
-            if kl['ekipa'] == ekipa1:
-                izid = str(kl['golivec']) + ':' + str(kl['golimanj'])
-                polepsan_slovar[izid] = float(slovar[kljuc])
-            elif kl['ekipa'] == 'Draw':
-                izid = str(kl['golivec']) + ':' + str(kl['golimanj'])
-                polepsan_slovar[izid] = float(slovar[kljuc])
-            elif kl['ekipa'] == ekipa2:
-                izid = str(kl['golimanj']) + ':' + str(kl['golivec'])
-                polepsan_slovar[izid] = float(slovar[kljuc])
-            else: print("Napaka pri branju imen ekip")
+    #vzorec_za_polepsanje_slovarja = re.compile(
+    #    r'(?P<ekipa>[a-zA-Z\s]*?)'
+    #    r' '
+    #    r'(?P<golivec>\d*?)'
+    #    r'-'
+    #    r'(?P<golimanj>\d*)',
+    #    flags=re.DOTALL)
+#
+    #polepsan_slovar = {}
+#
+    #for kljuc in slovar:
+    #    for kl in re.finditer(vzorec_za_polepsanje_slovarja, kljuc):
+    #        if kl['ekipa'] == ekipa1:
+    #            izid = str(kl['golivec']) + ':' + str(kl['golimanj'])
+    #            polepsan_slovar[izid] = float(slovar[kljuc])
+    #        elif kl['ekipa'] == 'Draw':
+    #            izid = str(kl['golivec']) + ':' + str(kl['golimanj'])
+    #            polepsan_slovar[izid] = float(slovar[kljuc])
+    #        elif kl['ekipa'] == ekipa2:
+    #            izid = str(kl['golimanj']) + ':' + str(kl['golivec'])
+    #            polepsan_slovar[izid] = float(slovar[kljuc])
+    #        else: print("Napaka pri branju imen ekip")
             
-    return(ekipa1, ekipa2, polepsan_slovar)
+    return(ekipa1, ekipa2, slovar)
