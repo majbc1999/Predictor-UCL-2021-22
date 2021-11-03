8cafa01ae5a8af86994d87b55f260e52af835714
majbc1999 <majbc1999@gmail.com>
2021-11-03 23:42:10 +0100

    zakljucen 4. matchday

--- orodja/napovedovanje.py
+++ orodja/napovedovanje.py
@@ -15,32 +15,32 @@ sez07 = ["3:0", "4:0", "3:1", "4:1"]
 sez08 = ["2:0", "1:0", "2:1", ""]
 
 sez09 = ["2:1", "1:1", "2:0", "1:0"]
-sez10 = ["2:0", "3:0", "3:1", "2:1"]
+sez10 = ["2:0", "3:0", "3:1", ""]
 sez11 = ["2:1", "3:1", "2:2", ""]
 sez12 = ["1:0", "2:0", "2:1", "1:1"]
-sez13 = ["3:0", "4:0", "2:0", ""]
+sez13 = ["3:0", "4:0", "2:0", "3:1"]
 sez14 = ["1:3", "1:2", "0:2", ""]
 sez15 = ["0:2", "1:2", "0:3", ""]
-sez16 = ["2:1", "2:2", "", ""]
+sez16 = ["2:1", "2:2", "1:2", ""]
 
 # Tukaj izračuna vse
 
 # Tekme 19.10.
-vrni_napoved(sez01, '01', matchday)
-vrni_napoved(sez02, '02', matchday)
-vrni_napoved(sez03, '03', matchday)
-vrni_napoved(sez04, '04', matchday)
-vrni_napoved(sez05, '05', matchday)
-vrni_napoved(sez06, '06', matchday)
-vrni_napoved(sez07, '07', matchday)
-vrni_napoved(sez08, '08', matchday)
+#vrni_napoved(sez01, '01', matchday)
+#vrni_napoved(sez02, '02', matchday)
+#vrni_napoved(sez03, '03', matchday)
+#vrni_napoved(sez04, '04', matchday)
+#vrni_napoved(sez05, '05', matchday)
+#vrni_napoved(sez06, '06', matchday)
+#vrni_napoved(sez07, '07', matchday)
+#vrni_napoved(sez08, '08', matchday)
 
 # Tekme 20.10.
-#vrni_napoved(sez09, '09', matchday)
-#vrni_napoved(sez10, '10', matchday)
-#vrni_napoved(sez11, '11', matchday)
-#vrni_napoved(sez12, '12', matchday)
-#vrni_napoved(sez13, '13', matchday)
-#vrni_napoved(sez14, '14', matchday)
-#vrni_napoved(sez15, '15', matchday)
-#vrni_napoved(sez16, '16', matchday)
+vrni_napoved(sez09, '09', matchday)
+vrni_napoved(sez10, '10', matchday)
+vrni_napoved(sez11, '11', matchday)
+vrni_napoved(sez12, '12', matchday)
+vrni_napoved(sez13, '13', matchday)
+vrni_napoved(sez14, '14', matchday)
+vrni_napoved(sez15, '15', matchday)
+vrni_napoved(sez16, '16', matchday)
