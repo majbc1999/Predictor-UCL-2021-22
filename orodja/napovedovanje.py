40d4e82ca15fd1eaabe47c72287eb0498a93aae8
majbc1999 <majbc1999@gmail.com>
2021-11-23 17:51:45 +0100

    5. matchday napovedi

--- orodja/napovedovanje.py
+++ orodja/napovedovanje.py
@@ -1,39 +1,39 @@
 from rezultat import vrni_napoved
 
 # Tukaj nastavimo matchday
-matchday = '4'
+matchday = '5'
 
 # Tukaj napišemo popularne rezultate
 
-sez01 = ["1:1", "1:2", "2:1", ""]
-sez02 = ["0:3", "0:2", "", "0:4"]
-sez03 = ["2:0", "2:1", "1:0", "1:1"]
-sez04 = ["2:0", "2:1", "3:1", "1:0"]
-sez05 = ["1:2", "1:3", "2:2", "2:1"]
-sez06 = ["0:2", "1:2", "0:1", "1:1"]
-sez07 = ["3:0", "4:0", "3:1", "4:1"]
-sez08 = ["2:0", "1:0", "2:1", ""]
+sez01 = ["1:2", "1:1", "2:1", "0:2"]
+sez02 = ["0:3", "0:4", "", "0:5"]
+sez03 = ["1:2", "1:1", "0:2", "0:1"]
+sez04 = ["0:2", "1:2", "1:3", ""]
+sez05 = ["1:1", "2:1", "1:2", "1:0"]
+sez06 = ["2:1", "2:0", "1:1", "1:0"]
+sez07 = ["2:1", "2:0", "1:1", "1:0"]
+sez08 = ["2:1", "2:0", "1:0", ""]
 
-sez09 = ["2:1", "1:1", "2:0", "1:0"]
-sez10 = ["2:0", "3:0", "3:1", ""]
-sez11 = ["2:1", "3:1", "2:2", ""]
-sez12 = ["1:0", "2:0", "2:1", "1:1"]
-sez13 = ["3:0", "4:0", "2:0", "3:1"]
-sez14 = ["1:3", "1:2", "0:2", ""]
-sez15 = ["0:2", "1:2", "0:3", ""]
-sez16 = ["2:1", "2:2", "1:2", ""]
+sez09 = ["0:2", "0:3", "1:3", "1:2"]
+sez10 = ["2:0", "2:1", "3:1", "3:0"]
+sez11 = ["1:2", "0:2", "0:1", "1:3"]
+sez12 = ["2:1", "2:0", "1:1", ""]
+sez13 = ["0:3", "0:2", "1:3", "1:2"]
+sez14 = ["1:2", "0:2", "1:3", ""]
+sez15 = ["2:1", "2:2", "1:2", ""]
+sez16 = ["3:0", "2:0", "3:1", ""]
 
 # Tukaj izračuna vse
 
 # Tekme 19.10.
-vrni_napoved(sez01, '01', matchday)
-vrni_napoved(sez02, '02', matchday)
-vrni_napoved(sez03, '03', matchday)
-vrni_napoved(sez04, '04', matchday)
-vrni_napoved(sez05, '05', matchday)
-vrni_napoved(sez06, '06', matchday)
-vrni_napoved(sez07, '07', matchday)
-vrni_napoved(sez08, '08', matchday)
+#vrni_napoved(sez01, '01', matchday)
+#vrni_napoved(sez02, '02', matchday)
+#vrni_napoved(sez03, '03', matchday)
+#vrni_napoved(sez04, '04', matchday)
+#vrni_napoved(sez05, '05', matchday)
+#vrni_napoved(sez06, '06', matchday)
+#vrni_napoved(sez07, '07', matchday)
+#vrni_napoved(sez08, '08', matchday)
 
 # Tekme 20.10.
 vrni_napoved(sez09, '09', matchday)
