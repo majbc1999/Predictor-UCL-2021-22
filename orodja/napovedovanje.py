039a9b1118b99f6adcf44901d51c34dabe9c7619
majbc1999 <majbc1999@gmail.com>
2022-02-21 16:21:31 +0100

    rahlo spremenjeni htmlji oddscheckerja

--- orodja/napovedovanje.py
+++ orodja/napovedovanje.py
@@ -1,32 +1,24 @@
 from rezultat import vrni_napoved
 
 # Tukaj nastavimo matchday
-matchday = '7'
+matchday = '8'
 
 # Tukaj napišemo popularne rezultate
 
-sez01 = ["2:1", "1:2", "1:1", ""]
-sez02 = ["0:3", "1:3", "0:2", ""]
-sez03 = ["1:2", "1:3", "0:2", ""]
-sez04 = ["0:3", "0:4", "1:3", "1:4"]
+sez01 = ["1:2", "1:1", "0:2", ""]
+sez02 = ["2:0", "3:0", "2:1", "3:1"]
+sez03 = ["1:2", "1:1", "2:2", "2:1"]
+sez04 = ["1:3", "1:2", "0:2", ""]
 
-#sez05 = ["0:3", "0:4", "1:3", "1:4"]
-#sez06 = ["2:1", "2:0", "3:1", "3:0"]
-#sez07 = ["1:2", "1:1", "0:2", "0:1"]
-#sez08 = ["2:0", "1:1", "2:1", "1:0"]
 
 
 # Tukaj izračuna vse
-# Tekme 15.02
+
+# Tekme 22.02
 vrni_napoved(sez01, '01', matchday)
 vrni_napoved(sez02, '02', matchday)
-# Tekme 16.02
+# Tekme 23.02
 vrni_napoved(sez03, '03', matchday)
 vrni_napoved(sez04, '04', matchday)
 
 
-
-#vrni_napoved(sez05, '05', matchday)
-#vrni_napoved(sez06, '06', matchday)
-#vrni_napoved(sez07, '07', matchday)
-#vrni_napoved(sez08, '08', matchday)
--- orodja/vrni_kvote_iz_html.py
+++ orodja/vrni_kvote_iz_html.py
@@ -23,8 +23,20 @@ def vrni_kvote_iz_html(datoteka, matchday):
         r'</button>'
     )
 
+    vzorec_za_rezulat3 = re.compile(
+        r';">'
+        r'(?P<goli1>\d+?)'
+        r'-'
+        r'(?P<goli2>\d+?)'
+        r'</p>(.*?)<button type="button" class="button_b1oycxy6">'
+        r'(?P<kvote>.*?)'
+        r'</button>'
+    )
+
     #vzorec_za_rezultat_vscode = <span class="float-wrap name-wrap"><span class="tcell"><div class="top-row"><a class="popup selTxt" target="_blank" title="View odds history for .*?" href=".*?" data-name=".*?".*?</a></div></span></span></td>.*?<td class="bc bs o.*?(\n)?.*?" data-bk="B3" data-odig=".*?" data-o=".*?" data-hcap=".*?" data-fodds=".*?" data-best-ew=".*?" data-best-wo=".*?"><p>.*?</p></td>"
     #vzorec2 = <p class="body-text-3 MarketExpanderBetName_m1m6ixsu">((\d*?)-(\d*?))</p>(.*?)<button type="button" class="button_b1oycxy6">(.*?)</button>
+    #vzorec3 = ;">((\d+?)-(\d+?))</p>(.*?)<button type="button" class="button_b1oycxy6">
+
 
     vzorec_za_ekipe = re.compile(
         r'</style><title>'
@@ -53,7 +65,7 @@ def vrni_kvote_iz_html(datoteka, matchday):
 
     i = 0
 
-    for razplet in re.finditer(vzorec_za_rezulat2, tekma):
+    for razplet in re.finditer(vzorec_za_rezulat3, tekma):
             if (razplet['goli1'] + ':' + razplet['goli2']) in slovar:
                 obrni = True
             if obrni:
@@ -75,3 +87,5 @@ def vrni_kvote_iz_html(datoteka, matchday):
         gostujoca_ekipa = ekipa['gostujoca_ekipa']
 
     return([domaca_ekipa, gostujoca_ekipa, slovar])
+
+#print(vrni_kvote_iz_html('01', '8'))
