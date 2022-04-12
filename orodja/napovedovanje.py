f90e5175ea57139b37f8742337d85fea30a72b45
majbc1999 <majbc1999@gmail.com>
2022-04-12 17:05:53 +0200

    napovedi ro8, 2. teden

--- orodja/napovedovanje.py
+++ orodja/napovedovanje.py
@@ -1,21 +1,21 @@
 from rezultat import vrni_napoved
 
 # Tukaj nastavimo matchday
-matchday = '11'
+matchday = '12'
 
 # Tukaj napišemo popularne rezultate
 
-sez01 = ["0:2", "1:3", "1:2", "0:3"]
-sez02 = ["2:1", "2:0", "3:1", ""]
-sez03 = ["1:3", "0:3", "0:2", "1:2"]
-sez04 = ["1:1", "1:2", "2:1", ""]
+sez01 = ["2:1", "2:0", "1:1", ""]
+sez02 = ["3:0", "2:0", "3:1", ""]
+sez03 = ["1:2", "1:1", "0:2", "0:1"]
+sez04 = ["2:0", "3:0", "2:1", "3:1"]
 
 
 # Rezultati prvih tekem (ne dopuščamo enajstmetrovk, ki niso najbolj verjetna stvar, zanemarimo, da v 120 min pade povprečno več zadetkov kakor po 90 min)
-r01 = [20, 0]
-r02 = [20, 0]
-r03 = [20, 0]
-r04 = [20, 0]
+r01 = [3, 1]
+r02 = [0, 1]
+r03 = [0, 1]
+r04 = [3, 1]
 
 
 # Tukaj izračuna vse
