9ad7ce7cbf3fc680f4e7b14555f6c616a47dfa78
majbc1999 <majbc1999@gmail.com>
2022-04-25 13:32:56 +0200

    napovedi 1. teden Semifinal

--- orodja/napovedovanje.py
+++ orodja/napovedovanje.py
@@ -1,31 +1,21 @@
 from rezultat import vrni_napoved
 
 # Tukaj nastavimo matchday
-matchday = '12'
+matchday = '13'
 
 # Tukaj napišemo popularne rezultate
 
-sez01 = ["2:1", "2:0", "1:1", ""]
-sez02 = ["3:0", "2:0", "3:1", ""]
-sez03 = ["1:2", "1:1", "0:2", "0:1"]
-sez04 = ["2:0", "3:0", "2:1", "3:1"]
+sez01 = ["2:1", "1:2", "1:1", ""]
+sez02 = ["2:0", "2:1", "3:1", "3:0"]
 
 
 # Rezultati prvih tekem (ne dopuščamo enajstmetrovk, ki niso najbolj verjetna stvar, zanemarimo, da v 120 min pade povprečno več zadetkov kakor po 90 min)
-r01 = [3, 1]
-r02 = [0, 1]
-r03 = [0, 1]
-r04 = [3, 1]
+r01 = [12, 0]
+r02 = [12, 0]
 
 
 # Tukaj izračuna vse
 
-# Tekme 22.02
+# Tekme
 vrni_napoved(sez01, '01', matchday, r01)
 vrni_napoved(sez02, '02', matchday, r02)
-
-# Tekme 23.02
-vrni_napoved(sez03, '03', matchday, r03)
-vrni_napoved(sez04, '04', matchday, r04)
-
-
