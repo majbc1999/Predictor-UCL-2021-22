5304c0237a10f7a2f145e06bf51ba43eae277fc2
majbc1999 <majbc1999@gmail.com>
2022-05-28 17:52:23 +0200

    napovedi za finale

--- orodja/napovedovanje.py
+++ orodja/napovedovanje.py
@@ -1,21 +1,18 @@
 from rezultat import vrni_napoved
 
 # Tukaj nastavimo matchday
-matchday = '14'
+matchday = '15'
 
 # Tukaj napišemo popularne rezultate
 
-sez01 = ["1:2", "1:1", "0:2", "1:3"]
-sez02 = ["2:1", "2:2", "3:1", "1:2"]
+sez01 = ["1:2", "2:1", "2:3", "1:3"]
 
 
 # Rezultati prvih tekem (ne dopuščamo enajstmetrovk, ki niso najbolj verjetna stvar, zanemarimo, da v 120 min pade povprečno več zadetkov kakor po 90 min)
-r01 = [0, 2]
-r02 = [0, 1]
+r01 = [0, 15]
 
 
 # Tukaj izračuna vse
 
 # Tekme
 vrni_napoved(sez01, '01', matchday, r01)
-vrni_napoved(sez02, '02', matchday, r02)
